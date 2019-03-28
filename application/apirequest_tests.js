8f4f79c1c3e91460e28199d9ca58d193c83ef3d8
ccolombijn <ccolombijn@gmail.com>
2019-03-28 13:56:08 +0100

    Added apiRoutes test

--- application/apirequest_tests.js
+++ application/apirequest_tests.js
@@ -1,26 +1,4 @@
 
-let t_response = { response : undefined }
-const baseUrl =  'http://localhost:8081/api/',
-data = {
-  id : 0,
-  name : 'test'
-},
-r_get = {url : `${baseUrl}players`, callback : t_res },
-t_get = xhr(r_get),
-r_post = {data : data,url : `${baseUrl}players`,method: 'POST',status : 201},
-t_post = xhr(r_post);
-
-//console.log(t_response)
-//r_delete = {url : `${baseUrl}players`,method: 'DELETE',status : 201},
-//t_delete = xhr(r_delete),
-//r_update = {data : data,url : `${baseUrl}players`,method: 'PUT',status : 201},
-//t_update = xhr(r_update)
-
-function t_res( event ){
-  t_response.response = event.target.responseText
-  console.log(event)
-}
-
 function xhr( args ){
   if(typeof args === 'string') args = { url : args }
   const xmlhttp = new XMLHttpRequest();
@@ -41,12 +19,12 @@ function xhr( args ){
 const request = {}
 const config = {
   server : 'localhost:8081',
-  api : 'api'
+  api : 'api',
   routes : [
-    { 
-      endpoint : 'players', 
-      key : 'id', 
-      methods : [ 'post','get','put','delete' ], 
+    {
+      endpoint : 'players',
+      key : 'id',
+      methods : [ 'post','get','put','delete' ],
       fields : [ 'name','score' ]
     }
   ]
@@ -59,25 +37,30 @@ const apiRoutes = ( config ) => {
     let args
     for(let method of item.methods){
       switch(method){
-        case 'post' : 
+        case 'post' :
           // request.players.post(args)
           request[item.endpoint][method] = (args)=> {
             args = { url : `${url}${item.endpoint}`, data : args.data,method: 'POST',status : 201, callback : args.callback}
-            
+
             xhr(args)
           }
           break;
-        case 'get' : 
+        case 'get' :
           request[item.endpoint][method] = (args)=> {
             args = { url : `${url}${item.endpoint}/${args.id}`, callback : args.callback}
-            
+
             xhr(args)
           }
           break;
-         
+
       }
     }
   }
 }
+
+apiRoutes( config )
+request.players.get({id : 1,callback : (event) => {
+  console.log(event.target.responseText)
+}})
 
 //model.apiRequest( {endpoint : 'players', callback : ( event, args) => console.log( event.target.responseText ) })
