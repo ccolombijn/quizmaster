7f8cc63ecaf59f77d2d238adc41ed3699db2f8ae
ccolombijn <ccolombijn@gmail.com>
2019-03-28 11:13:56 +0100

    Updated REST API files

--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
-
 "use strict";
 
 const api = (function(){
@@ -7,7 +6,8 @@ const api = (function(){
   const express = require( 'express' )
   const app = express()
   const bodyParser = require( 'body-parser' )
-  const _ = require( 'lodash' )
+  const fs = require('fs')
+
 
   app.use( bodyParser.json() )
   app.use( function(req, res, next) {
@@ -17,24 +17,31 @@ const api = (function(){
     next()
   })
   // config
-  const config = {
+  const config = process.argv[3]
+  ? fs.readFile( `${process.argv[3]}.json`, (err, data)=>JSON.parse(data))
+  : {
     db : {
       host: 'localhost',
       user: 'root',
       password: 'root',
       database: 'quiz'
-    },routes : [
+    },
+    routes : [
 
-      { route : 'players' }, //setApi.getAll
-      { route : 'players/:id' }, // setApi.getOne
-      { route : 'players/:id', method : 'delete' }, // setApi.delete
-      { route : 'players', method : 'post' }, // setApi.post
-      { route : 'players', method : 'put', fields : ['name','score'] }
-
-
-      //'players', 'players/:id', 'game','game/:id','questions','questions/:id','anwsers','anwsers/:id'
+      /*{ route : 'players' }, //set.getAll
+      { route : 'players/:id' , method : 'get' }, // set.getOne
+      { route : 'players/:id', method : 'delete' }, // set.delete
+      { route : 'players', method : 'post' }, // set.post
+      { route : 'players/:id', method : 'put', fields : ['name','score'] }*/  // set.put
+      { route : 'players',
+        methods : [ 'get', 'delete', 'post', 'put' ],
+        key : 'id',
+        fields : ['name','score']
+      }
     ]
   }
+
+
   const connection = mysql.createConnection( config.db )
   connection.connect((err) => {
     if (err) {
@@ -45,14 +52,16 @@ const api = (function(){
   });
 
 
-  const setApi= (( args ) => {
+  const set = (() => {
+    const pre = config.prefix ? config.prefix : 'api'
+// get
 // .............................................................................
     const getOne = ( route ) => {
-      app.get( `/api/${route}`, function( req, res ) {
+      app.get( `/${pre}/${route}`, function( req, res ) {
+        route = route.split('/:')
+        let key = +req.params[ route[1] ]
 
-        let id = +req.params[ 'id' ]
-
-        connection.query(`SELECT * FROM ${route.split('/')[0]} where id=?`, id, ( err, rows ) => {
+        connection.query(`SELECT * FROM ${route[0]} where ${route[1]}=?`, key, ( err, rows ) => {
           if (!err) {
             let record = rows[0];
             res.setHeader('Content-Type', 'application/json')
@@ -62,16 +71,14 @@ const api = (function(){
           }
         })
       });
-
-
     }
 
     const getAll = ( route ) => {
-      app.get(`/api/players`, function(req, res) {
+      app.get( `/${pre}/${route}`, function(req, res) {
 
         res.setHeader('Content-Type', 'application/json')
 
-        connection.query(`SELECT * FROM players`, ( err, records ) => {
+        connection.query(`SELECT * FROM ${route}`, ( err, records ) => {
           if (!err) {
             res.end( JSON.stringify(records) )
           } else {
@@ -81,44 +88,40 @@ const api = (function(){
       });
     }
 
-  const _get = ( route ) => {
-    route.includes( '/:id' ) ? getOne( route ) : getAll( route )
-  }
+  const _get = ( route ) => route.includes( '/:id' ) ? getOne( route ) : getAll( route )
 
 
+// put
 // .............................................................................
   const _put = (route, fields) => {
-    app.put(`/api/${route}/:id`, function(req, res) {
-
+    app.put(`/${pre}/${route}`, function(req, res) {
+          route = route.split('/:')
           // First read id from params
-          let id = +req.params.id
+          let id = +req.params[route[1]]
           let body = req.body;
-          let query = 'UPDATE players SET '
-          for( let field of fields){
-            query =+ ''
+          let query = `UPDATE ${route[0]} SET `
+          for( let field in fields){
+            query =+ `${fields[field]}=?`
+            if( field != fields.length ) query =+ ','
           }
-
-          connection.query(
-            query,
-            [body.anwser, id],
-            (err, result) => {
+          query =+ ` WHERE ${route[1]}=?`
+          connection.query(query,[body.item, id], (err, result) => {
               if (!err) {
                 console.log(`Changed ${result.changedRows} row(s)`);
 
                 // end of the update => send response
                 // execute a query to find the result of the update
-                connection.query('SELECT * FROM players where id=?', [id], (err, rows) => {
+                connection.query(`SELECT * FROM ${route[0]} where ${route[1]}=?`, [id], (err, rows) => {
                   if (!err) {
                     console.log('Data received from Db:\n');
 
-                    let user = rows[0];
+                    let record = rows[0];
 
-                    console.log(user);
-                    if (user) {
+                    console.log(record);
+                    if (record) {
                       res.setHeader('Content-Type', 'application/json')
-                      res.end(JSON.stringify(user));
+                      res.end(JSON.stringify(record));
                     } else {
-                      res.setHeader('Content-Type', 'application/json')
                       console.log("Not found!!!");
                       res.status(404).end(); // rloman send 404???
                     }
@@ -139,16 +142,16 @@ const api = (function(){
   const _post = (route) => {
     app.post(`/api/${route}`, function(req, res) {
 
-      let player = req.body;
-      connection.query(`INSERT INTO ${route} SET ?`, player, (err, result) => {
+
+      connection.query(`INSERT INTO ${route} SET ?`, req.body, (err, result) => {
         if (!err) {
           res.setHeader('Content-Type', 'application/json')
-          connection.query('SELECT * FROM players where id=?', result.insertId, (err, rows) => {
+          connection.query(`SELECT * FROM ${route} where id=?`, result.insertId, (err, rows) => {
             if (!err) {
-              let player = rows[0];
-              if (player) {
+              let record = rows[0];
+              if (record) {
                 res.setHeader('Content-Type', 'application/json')
-                res.status(201).end(JSON.stringify(player));
+                res.status(201).end(JSON.stringify(record));
               } else {
                 res.setHeader('Content-Type', 'application/json')
                 res.status(404).end();
@@ -164,12 +167,12 @@ const api = (function(){
     });
 
   }
-  // .............................................................................
+// .............................................................................
   const _delete = (route) => {
     app.delete('/api/players/:id', function( req, res ) {
       let id =+req.params.id;
 
-      connection.query( `DELETE FROM players WHERE id = ?`, [id], ( err, result ) => {
+      connection.query( `DELETE FROM ${route} WHERE id = ?`, [id], ( err, result ) => {
         if (!err) {
           res.status(204).end();
         } else {
@@ -179,52 +182,58 @@ const api = (function(){
     });
   }
 
-    return {
-      get : _get,
-      put : _put,
-      post : _post,
-      delete : _delete
-    }
-  })()
+  return {
+    get : _get,
+    put : _put,
+    post : _post,
+    delete : _delete
+  }
+})()
 
-
-
-
-
-
-
-  const api = setApi
-  for( let item of config.routes ){
-
-    if(  item.route.split(':')[1] && item.method ){ //delete
+const setRoutes = (function(){
+  /*const setMethod = (item) => {
+    if(  item.route.split(':')[1] && item.method === 'delete'){ //delete
       console.log(`${item.route.split('/')[0]} delete`)
-      setApi.delete( item.route )
+      set.delete( item.route )
 
-    }else if ( item.route.split(':')[1]) { // getOne
+    }else if ( item.route.split(':')[1] && item.method === 'get' ) { // getOne
       console.log(`${item.route.split('/')[0]} getOne`)
-      setApi.get( item.route )
+      set.get( item.route )
 
-    }else if (item.method && item.fields ) { // put
+    }else if (item.method === 'put' && item.fields ) { // put
       console.log(`${item.route} put`)
-      setApi.put( item.route, item.fields )
+      set.put( item.route, item.fields )
 
-    }else if (item.method  ) { // post
+    }else if (item.method === 'post' ) { // post
       console.log(`${item.route} post`)
-      setApi.post( item.route )
+      set.post( item.route )
 
     }else { // getAll
       console.log(`${item.route} getAll`)
-      setApi.get( item.route )
+      set.get( item.route )
 
     }
+  }*/
 
+  for( let item of config.routes ){
+    //setMethod( item )
+    for( let method of item.methods) {
+      
+      let fields = ( method === 'put') ? ' '+item.fields.join(',') : ''
+
+      let route = ( method === 'get' || method === 'delete' || method === 'put' )
+      ? `${item.route}/:${item.key}`
+      : item.route
+      console.log( `api.set.${method} ${route} ${fields}`)
+      item.fields ? set[method](route,item.fields) :  set[method](route)
+    }
   }
+  // for( let item of config.routes ) set[item.method](item.route,item.fields)
+ })()
 
 
   const server = app.listen(8081, () => {
     console.log( 'Server listening on port 8081')
   })
-
-
 
 })()
